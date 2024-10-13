6cf90e46c88a8e65daf3182514f4e344305c029a
Daniel <daniel@macbookpro.home>
2024-10-13 01:32:55 +0100

    Added recipies_router

--- api/main.py
+++ api/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
 from routes.users import users_router
+from routes.recipes import recipes_router
 from routes.registration import registration_router
 
 api = FastAPI()
 
 api.include_router(users_router)
 api.include_router(registration_router)
+api.include_router(recipes_router)
 
 @api.get("/")
 async def root():
