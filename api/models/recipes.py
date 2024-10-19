14a00d3b33fd5f87e0630774d254829b0fb61a58
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-10-19 15:47:41 +0100

    Created UpdateRecipe, made CreateRecipe a subclass of RecipeOptionalAttributes

--- api/models/recipes.py
+++ api/models/recipes.py
@@ -1,4 +1,34 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class GenerateRecipe(BaseModel):
     recipe_url: str
+
+class RecipeOptionalAttributes(BaseModel):
+    Servings: Optional[int] = None
+    Cook_Time_In_Minutes: Optional[int] = None
+    Calories: Optional[int] = None
+    Carbs: Optional[int] = None
+    Protein: Optional[int] = None
+    Fats: Optional[int] = None
+    Sugars: Optional[int] = None
+    Recipe_URL: Optional[str] = None
+
+class CreateRecipe(RecipeOptionalAttributes):
+    Recipe_Name: str
+
+
+# class CreateRecipe(BaseModel):
+#     Recipe_Name: str
+#     Servings: Optional[int] = None
+#     Cook_Time_In_Minutes: Optional[int] = None
+#     Calories: Optional[int] = None
+#     Carbs: Optional[int] = None
+#     Protein: Optional[int] = None
+#     Fats: Optional[int] = None
+#     Sugars: Optional[int] = None
+#     Recipe_URL: Optional[str] = None
+#
+
+class UpdateRecipe(RecipeOptionalAttributes):
+    Recipe_Name: Optional[str] = None
