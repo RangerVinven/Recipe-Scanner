21bcf8910e5aa97d1d1ccb0c55645ec349fea6d3
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-10-19 15:48:57 +0100

    Added CRUD operations for recipes

--- api/routes/recipes.py
+++ api/routes/recipes.py
@@ -8,19 +8,106 @@ import urllib.request, urllib.error, urllib.parse
 from time import sleep
 from dotenv import load_dotenv
 
-from sqlalchemy import insert
+from sqlalchemy import insert, select, delete, update
 from fastapi import APIRouter, HTTPException, Response, Request
 
-from models.recipes import GenerateRecipe
+from models.recipes import GenerateRecipe, CreateRecipe, UpdateRecipe
 from models.tables import recipes_table, ingredients_table, steps_table
 
 from utils.database_connector import connection
 from utils.openai_connector import openai_client
+from utils.query_manager import insert_data
 from utils.request_manager import get_session_token, get_user_id
 
 load_dotenv()
 
-recipes_router = APIRouter()
+recipes_router = APIRouter(prefix="/recipes")
+
+# Gets all the recipes belonging to a user
+@recipes_router.get("/")
+def get_recpies(request: Request):
+    # Gets the user_id associated with the session token
+    user_id = get_user_id(request)
+
+    # Runs the select query
+    select_query = select(recipes_table).where(recipes_table.c.User_ID == user_id)
+    results = connection.execute(select_query)
+    results_json = results.mappings().all()
+
+    return results_json
+
+# Gets a recipe
+@recipes_router.get("/{recipe_id}")
+def get_recipe(request: Request, recipe_id: int):
+
+    # Gets the user_id associated with the session token
+    user_id = get_user_id(request)
+
+    # Selects the recipe with a matching user_id and recipe_id
+    select_query = select(recipes_table).where(recipes_table.c.Recipe_ID == recipe_id).where(recipes_table.c.User_ID == user_id)
+    results = connection.execute(select_query)
+    results_json = results.mappings().all()
+
+    # If the recipe exists
+    if len(results_json) > 0:
+        return results_json[0]
+
+    raise HTTPException(status_code=404)
+
+
+# Creates a recipe
+@recipes_router.post("/")
+def create_recipe(request: Request, recipe: CreateRecipe):
+    # Gets the user_id and adds it to the recipe
+    user_id = get_user_id(request)
+    recipe_json = dict(recipe)
+    recipe_json["User_ID"] = user_id
+
+    recipe_id = insert_data(recipes_table, recipe_json)
+    print(recipe_id)
+
+
+    return
+
+# Updates a recipe
+@recipes_router.patch("/{recipe_id}")
+def update_recipe(request: Request, recipe_id: int, recipe: UpdateRecipe):
+    # Gets the user id
+    user_id = get_user_id(request)
+    
+    # Changes removes the None values from the dictionary
+    # Removes none values from the user's data,
+    # Taken from: https://blog.finxter.com/5-best-ways-to-remove-null-values-from-a-python-dictionary/
+    data = {k: v for k, v in vars(recipe).items() if v is not None}
+
+    # Updates the recipe data
+    update_query = update(recipes_table).where(recipes_table.c.Recipe_ID == recipe_id).where(recipes_table.c.User_ID == user_id).values(dict(data))
+    results = connection.execute(update_query)
+    connection.commit()
+
+    # If the recipe doesn't exist, or if the user doesn't own it
+    if results.rowcount == 0:
+        raise HTTPException(status_code=404)
+
+    return
+
+
+# Deletes a recipe
+@recipes_router.delete("/{recipe_id}")
+def delete_recipe(request: Request, recipe_id: int):
+    # Gets the user id
+    user_id = get_user_id(request)
+
+    # Attempts to delete the recipe 
+    delete_query = delete(recipes_table).where(recipes_table.c.User_ID == user_id).where(recipes_table.c.Recipe_ID == recipe_id)
+    results = connection.execute(delete_query)
+    connection.commit()
+
+    if results.rowcount == 0:
+        raise HTTPException(status_code=404)
+
+    return
+
 
 # Uploads a file to OpenAI
 async def upload_file(file_name):
@@ -32,7 +119,7 @@ async def upload_file(file_name):
 
 @recipes_router.post("/extract_recipe")
 async def generate_recipe(recipe: GenerateRecipe, request: Request):
-    # Downloads the HTML of recipe's website 
+    # Gets the HTML of recipe's website 
     urllib_request = urllib.request.Request(recipe.recipe_url, data=None, headers={
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
     })
@@ -110,8 +197,7 @@ async def generate_recipe(recipe: GenerateRecipe, request: Request):
     steps_table_data = extracted_recipe["Steps"]
 
     # Gets the user id
-    session_token = get_session_token(request)
-    user_id = get_user_id(session_token)
+    user_id = get_user_id(request)
 
     # Adds the User ID to the recipe_table_data
     recipe_table_data["User_ID"] = user_id 
