d99ef2855cb10a0ccc16e9d81fc6c9fcbd68916c
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-10-23 20:18:07 +0100

    Added the steps_router

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from routes.users import users_router
+from routes.steps import steps_router
 from routes.recipes import recipes_router
 from routes.registration import registration_router
 
@@ -9,6 +10,7 @@ api = FastAPI()
 api.include_router(users_router)
 api.include_router(registration_router)
 api.include_router(recipes_router)
+api.include_router(steps_router)
 
 @api.get("/")
 async def root():
