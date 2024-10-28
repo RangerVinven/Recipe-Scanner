93ae8c12d253577fce33db6e11a47518ed1541a0
Daniel <daniel@daniels-MacBook-Pro.local>
2024-10-28 11:19:46 +0000

    Finished creating the CRUD endpoints

--- api/routes/ingredients.py
+++ api/routes/ingredients.py
@@ -21,7 +21,7 @@ def authorise_request(user_id, recipe_id):
 
 # Returns all the steps for a given recipe
 @ingredients_router.get("/{recipe_id}")
-def get_steps(recipe_id: int, request: Request):
+def get_ingredients(recipe_id: int, request: Request):
     user_id = get_user_id(request)
 
     # Runs the select query
@@ -31,48 +31,48 @@ def get_steps(recipe_id: int, request: Request):
 
     return results
 
-# @ingredients_router.post("/{recipe_id}")
-# def create_steps(steps: CreateSteps, request: Request, recipe_id: int):
-#     user_id = get_user_id(request)
-#     authorise_request(user_id, recipe_id)
-#
-#     # Ensures there's at least one step
-#     if len(steps.Steps) == 0:
-#         raise HTTPException(status_code=400)
-#
-#     # Turns the steps into a pure JSON object (no custom objects)
-#     steps = [dict(step) for step in steps.Steps]
-#
-#     # Adds the recipe_id to each of the objects in the steps array
-#     recipe_id_json = {"Recipe_ID": recipe_id}
-#     steps = [{**recipe_id_json, **step} for step in steps]
-#
-#     # Inserts the steps
-#     insert_query = insert(steps_table).values(steps)
-#     results = connection.execute(insert_query)
-#     connection.commit()
-#
-#     # If the steps weren't inserted weren't
-#     if results.rowcount == 0:
-#         raise HTTPException(status_code=500)
-#
-#     return
-#
-# @ingredients_router.put("/{recipe_id}")
-# def update_steps(recipe_id: int, request: Request, steps: UpdateSteps):
-#     user_id = get_user_id(request)
-#     authorise_request(user_id, recipe_id)
-#
-#     # Loops through the steps and updates them
-#     for step in steps.Steps:
-#         step = dict(step)
-#
-#         update_query = update(steps_table).where(steps_table.c.Step_ID == step["Step_ID"]).where(steps_table.c.Recipe_ID == recipe_id).values(step)
-#         connection.execute(update_query)
-#
-#     connection.commit()
-#
-#
+@ingredients_router.post("/{recipe_id}")
+def create_ingredients(ingredients: CreateIngredients, request: Request, recipe_id: int):
+    user_id = get_user_id(request)
+    authorise_request(user_id, recipe_id)
+
+    # Ensures there's at least one ingredient
+    if len(ingredients.Ingredients) == 0:
+        raise HTTPException(status_code=400)
+
+    # Turns the ingredients into a pure JSON object (no custom objects)
+    ingredients = [dict(ingredient) for ingredient in ingredients.Ingredients]
+
+    # Adds the recipe_id to each of the objects in the ingredients array
+    recipe_id_json = {"Recipe_ID": recipe_id}
+    ingredients = [{**recipe_id_json, **ingredient} for ingredient in ingredients]
+
+    # Inserts the ingredients
+    insert_query = insert(ingredients_table).values(ingredients)
+    results = connection.execute(insert_query)
+    connection.commit()
+
+    # If the ingredients weren't inserted weren't
+    if results.rowcount == 0:
+        raise HTTPException(status_code=500)
+
+    return
+
+@ingredients_router.put("/{recipe_id}")
+def update_ingredients(recipe_id: int, request: Request, ingredients: UpdateIngredients):
+    user_id = get_user_id(request)
+    authorise_request(user_id, recipe_id)
+
+    # Loops through the steps and updates them
+    for ingredient in ingredients.Ingredients:
+        ingredients = dict(ingredient)
+
+        update_query = update(ingredients_table).where(ingredients_table.c.Ingredient_ID == ingredient["Ingredient_ID"]).where(ingredients_table.c.Recipe_ID == recipe_id).values(ingredient)
+        connection.execute(update_query)
+
+    connection.commit()
+    return
+
 @ingredients_router.delete("/{ingredient_id}")
 def delete_step(ingredient_id: int, request: Request):
     user_id = get_user_id(request)
